5119f6fe5766c18bd215d98467fc9e70bda5cb3b
jose-dev1 <jose.cm25@hotmail.com>
2024-10-08 11:52:23 -0500

    counter change

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Counter: React.FC = () => {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div>
-            <h2>Contador: {count}</h2>
-            <button onClick={() => setCount(count + 1)}>Incrementar</button>
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+    </div>
+  );
 };
 
 export default Counter;
